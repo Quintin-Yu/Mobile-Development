1505448936a76b665400d575c38e7ec23ad46f24
Quintin Yu <Quintinyu02072@gmail.com>
2020-03-07 18:26:58 +0100

    Yes I am the greatest. Level 2.2 is finished

--- Level_2/Level_2_2/app/src/main/java/com/example/swipequiz/MainActivity.kt
+++ Level_2/Level_2_2/app/src/main/java/com/example/swipequiz/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.swipequiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,11 +19,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        questions.add(Questions("A \'val\' and \'var\' are the same."))
-        questions.add(Questions("Mobile Application Development grants 12 ECTS"))
-        questions.add(Questions("A Unit in Kotlin corresponds to a void in Java"))
-        questions.add(Questions("In Kotlin \'when\' replaces the \'switch\' operator in Java"))
-
         initViews()
     }
 
@@ -29,5 +26,60 @@ class MainActivity : AppCompatActivity() {
         tvRecycler.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
         tvRecycler.adapter = viewAdapter
         tvRecycler.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+
+        //Add Questions here
+        questions.add(Questions(getString(R.string.valVSvar), false))
+        questions.add(Questions(getString(R.string.ECTS), false))
+        questions.add(Questions(getString(R.string.KotlinJava), true))
+        questions.add(Questions(getString(R.string.whenSwitch), true))
+        questions.add(Questions(getString(R.string.ofcourseIamTheGreatest), true))
+
+        createItemTouchHelper().attachToRecyclerView(tvRecycler)
     }
+
+    private fun createItemTouchHelper(): ItemTouchHelper{
+        val callback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+
+                when (direction){
+                    ItemTouchHelper.LEFT ->
+                        if(!questions[position].questionTrue){
+                            correctAnswerSwiped(true)
+                            questions.removeAt(position)
+                        } else {
+                            correctAnswerSwiped(false)
+                        }
+
+                    ItemTouchHelper.RIGHT ->
+                        if(questions[position].questionTrue){
+                            correctAnswerSwiped(true)
+                            questions.removeAt(position)
+                        } else {
+                            correctAnswerSwiped(false)
+                        }
+                }
+
+                viewAdapter.notifyDataSetChanged()
+            }
+        }
+        return ItemTouchHelper(callback)
+    }
+
+    private fun correctAnswerSwiped(questionCorrect: Boolean){
+        if(questionCorrect){
+            Toast.makeText(this, getString(R.string.correctFeedback), Toast.LENGTH_SHORT).show()
+        } else{
+            Toast.makeText(this, getString(R.string.incorrectFeedback),Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
