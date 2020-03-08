8c24304c9ecdde136d152c5553b889ee2537e335
Quintin Yu <Quintinyu02072@gmail.com>
2020-03-08 17:47:19 +0100

    Step 2.3

--- Level_3/UserProfile/app/src/main/java/com/example/userprofile/SplashActivity.kt
+++ Level_3/UserProfile/app/src/main/java/com/example/userprofile/SplashActivity.kt
@@ -1,12 +1,23 @@
 package com.example.userprofile
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+           startActivity(
+               Intent(
+                   this@SplashActivity, CreateProfileActivity::class.java
+               )
+           )
+            finish()
+        }, 1000)
     }
 }
