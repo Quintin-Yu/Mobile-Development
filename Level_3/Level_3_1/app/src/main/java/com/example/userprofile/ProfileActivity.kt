2a7601aaad04687dd2324df0e0506bbda9075cb5
Quintin Yu <Quintinyu02072@gmail.com>
2020-03-08 20:51:28 +0100

    REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- Level_3/Level_3_1/app/src/main/java/com/example/userprofile/ProfileActivity.kt
+++ Level_3/Level_3_1/app/src/main/java/com/example/userprofile/ProfileActivity.kt
@@ -1,8 +1,7 @@
 package com.example.userprofile
 
-import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
 class ProfileActivity : AppCompatActivity() {
 
