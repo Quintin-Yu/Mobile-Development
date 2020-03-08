090794ece572cde7de2613a5769acb7876b13e6b
Quintin Yu <Quintinyu02072@gmail.com>
2020-03-08 20:20:55 +0100

    3.4

--- Level_3/Level_3_1/app/src/main/java/com/example/userprofile/CreateProfileActivity.kt
+++ Level_3/Level_3_1/app/src/main/java/com/example/userprofile/CreateProfileActivity.kt
@@ -1,6 +1,8 @@
 package com.example.userprofile
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_create_profile.*
@@ -8,6 +10,8 @@ import kotlinx.android.synthetic.main.activity_create_profile.*
 const val GALLERY_REQUEST_CODE = 100
 
 class CreateProfileActivity : AppCompatActivity() {
+
+    private var profileImageUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +27,16 @@ class CreateProfileActivity : AppCompatActivity() {
         val galleryIntent = Intent(Intent.ACTION_PICK)
         galleryIntent.type = "image/*"
         startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+        if(resultCode == Activity.RESULT_OK){
+            when(requestCode){
+                GALLERY_REQUEST_CODE ->{
+                    profileImageUri = data?.data
+                    ivProfilePicture.setImageURI(profileImageUri)
+                }
+            }
+        }
     }
 }
