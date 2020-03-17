68d6f4706bcf8ca63533825134251e946a209f61
Quintin Yu <Quintinyu02072@gmail.com>
2020-03-17 13:30:38 +0100

    All layouts are now complete i think

--- Level_4/Level_4_2/app/src/main/java/com/example/rockpaperscissors/MatchHistoryActivity.kt
+++ Level_4/Level_4_2/app/src/main/java/com/example/rockpaperscissors/MatchHistoryActivity.kt
@@ -1,12 +1,25 @@
 package com.example.rockpaperscissors
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_match_history.*
 
 class MatchHistoryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_match_history)
+
+        initViews()
+    }
+
+    private fun initViews(){
+        btnReturn.setOnClickListener{ onReturnClick() }
+    }
+
+    private fun onReturnClick(){
+        val returnIntent = Intent(this, MainActivity::class.java)
+        startActivity(returnIntent)
     }
 }
