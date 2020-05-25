ff359296800bff8ffa7aeb45c438f9e59fcca788
Quintin Yu <Quintinyu02072@gmail.com>
2020-05-25 18:28:12 +0200

    ?
    Id in RoomDatabase for somereason does oof

--- Level_5/Level_5_1/app/src/main/java/com/example/mypersonalnotepad/database/Note.kt
+++ Level_5/Level_5_1/app/src/main/java/com/example/mypersonalnotepad/database/Note.kt
@@ -1,7 +1,6 @@
 package com.example.mypersonalnotepad.database
 
 import android.os.Parcelable
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
@@ -11,17 +10,11 @@ import java.util.*
 @Entity(tableName = "noteTable")
 data class Note(
 
-    @ColumnInfo(name = "title")
     var title: String,
-
-    @ColumnInfo(name = "lastUpdated")
     var lastUpdated: Date,
-
-    @ColumnInfo(name = "text")
     var text: String,
 
-    @ColumnInfo(name = "title")
     @PrimaryKey(autoGenerate = true)
-    var id: Long
+    var id: Long?
 
 ) : Parcelable
--- Level_5/Level_5_1/app/src/main/java/com/example/mypersonalnotepad/database/NotepadRoomDatabase.kt
+++ Level_5/Level_5_1/app/src/main/java/com/example/mypersonalnotepad/database/NotepadRoomDatabase.kt
@@ -1,12 +1,20 @@
 package com.example.mypersonalnotepad.database
 
 import android.content.Context
-import androidx.room.Room
-import androidx.room.RoomDatabase
+import androidx.room.*
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.example.mypersonalnotepad.Converters
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.*
+
+@Database(entities = [Note::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 
 abstract class NotepadRoomDatabase : RoomDatabase() {
 
-    abstract fun noteDao() : NoteDao
+    abstract fun noteDao(): NoteDao
 
     companion object {
         private const val DATABASE_NAME = "NOTEPAD_DATABASE"
@@ -14,22 +22,31 @@ abstract class NotepadRoomDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: NotepadRoomDatabase? = null
 
-        fun getDatabase(context: Context) : NotepadRoomDatabase? {
-            if(INSTANCE == null) {
-                synchronized(NotepadRoomDatabase::class.java){
+        fun getDatabase(context: Context): NotepadRoomDatabase? {
+            if (INSTANCE == null) {
+                synchronized(NotepadRoomDatabase::class.java) {
                     if (INSTANCE == null) {
                         INSTANCE = Room.databaseBuilder(
                             context.applicationContext,
-                            NotepadRoomDatabase::class.java,
-                            DATABASE_NAME
+                            NotepadRoomDatabase::class.java, DATABASE_NAME
                         )
                             .fallbackToDestructiveMigration()
+                            .addCallback(object : RoomDatabase.Callback() {
+                                override fun onCreate(db: SupportSQLiteDatabase) {
+                                    super.onCreate(db)
+                                    INSTANCE?.let { database ->
+                                        CoroutineScope(Dispatchers.IO).launch {
+                                            database.noteDao().insertNote(Note("Title", Date(), ""))
+                                        }
+                                    }
+                                }
+                            })
                             .build()
                     }
                 }
             }
-
             return INSTANCE
         }
+
     }
 }
