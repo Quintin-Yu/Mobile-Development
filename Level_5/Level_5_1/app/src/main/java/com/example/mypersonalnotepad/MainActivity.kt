b91ee71f255837715035dc74f6aba445f9d9c67a
Quintin Yu <Quintinyu02072@gmail.com>
2020-05-25 18:47:17 +0200

    Viewmodel Main

--- Level_5/Level_5_1/app/src/main/java/com/example/mypersonalnotepad/MainActivity.kt
+++ Level_5/Level_5_1/app/src/main/java/com/example/mypersonalnotepad/MainActivity.kt
@@ -6,19 +6,42 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainActivityViewModel : MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        initViews()
+        initViewModel()
+    }
+
+    private fun initViews(){
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
     }
+
+    private fun initViewModel() {
+        mainActivityViewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        mainActivityViewModel.note.observe(this, Observer { note ->
+            if (note != null) {
+                tvTitle.text = note.title
+                tvDate.text = getString(R.string.last_updated, note.lastUpdated.toString())
+                tvInformation.text = note.text
+            }
+        })
+    }
+
 }
