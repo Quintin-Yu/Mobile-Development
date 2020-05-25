08e99e0607493bd0649c54798e92b6e611f4d09c
Quintin Yu <Quintinyu02072@gmail.com>
2020-05-25 17:31:29 +0200

    Set-up edit scene

--- Level_5/Level_5_1/app/src/main/java/com/example/mypersonalnotepad/EditActivity.kt
+++ Level_5/Level_5_1/app/src/main/java/com/example/mypersonalnotepad/EditActivity.kt
@@ -1,24 +1,22 @@
 package com.example.mypersonalnotepad
 
-import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_edit.*
 
-class MainActivity : AppCompatActivity() {
+class EditActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_edit)
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
     }
+
 }
