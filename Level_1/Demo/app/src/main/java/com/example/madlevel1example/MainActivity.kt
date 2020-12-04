575b6bb11440dff885f5109d8d1c09988313ca47
Quintin Yu <Quintinyu02072@gmail.com>
2020-12-04 14:53:17 +0100

    Demo level 1 completed

--- Level_1/Demo/app/src/main/java/com/example/madlevel1example/MainActivity.kt
+++ Level_1/Demo/app/src/main/java/com/example/madlevel1example/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.madlevel1example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.madlevel1example.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnConfirm.setOnClickListener{
+            checkAnswer()
+        }
+    }
+
+    private fun checkAnswer(){
+        val answer = binding.etGuess.text.toString()
+
+        val message = if(answer == getString(R.string.giraffe)){
+            getString(R.string.correct)
+        } else {
+            getString(R.string.incorrect)
+        }
+
+        Toast.makeText(this,message, Toast.LENGTH_LONG).show()
     }
 }
